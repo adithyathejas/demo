f42dddb61a6d340155c422a2871da964773101a7
adithyathejas <adithyathejus00@gmail.com>
2022-08-11 15:31:25 +0530

    last change

--- demo.js
+++ demo.js
@@ -17,3 +17,9 @@ addItem.style.color="green"
 */
 
 var items = document.getElementsByClassName('list-group-item');
+
+items[2].style.backgroundColor = "green"
+
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight="bold"
+}
