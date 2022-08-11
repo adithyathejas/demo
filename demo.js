21bb202127283c326b9a6823d6cc6a16b286f008
adithyathejas <adithyathejus00@gmail.com>
2022-08-11 15:22:11 +0530

    classname

--- demo.js
+++ demo.js
@@ -6,6 +6,14 @@ console.log(document.doctype)
 
 //getElementbyID//
 
+/*
 var headerTitle = document.getElementById('header-title')
 console.log(headerTitle);
 headerTitle.style.borderBottom="solid 3px #000"
+
+var addItem = document.getElementById('items')
+addItem.style.fontWeight="Bold"
+addItem.style.color="green"
+*/
+
+var items = document.getElementsByClassName('list-group-item');
