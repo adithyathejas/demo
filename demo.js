1085a10feade51d472d19409b7c32830286a8760
adithyathejas <adithyathejus00@gmail.com>
2022-08-11 15:12:59 +0530

    working with get element by id

--- demo.js
+++ demo.js
@@ -1,1 +1,11 @@
-console.dir("document")
+console.dir(document)
+console.log(document.domain)
+console.log(document.title)
+document.title = "123"
+console.log(document.doctype)
+
+//getElementbyID//
+
+var headerTitle = document.getElementById('header-title')
+console.log(headerTitle);
+headerTitle.style.borderBottom="solid 3px #000"
