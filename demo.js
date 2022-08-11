dc8b7148260115afd04b51d673f76d7ea08e4a77
adithyathejas <adithyathejus00@gmail.com>
2022-08-11 15:41:34 +0530

    tagname tried

--- demo.js
+++ demo.js
@@ -1,8 +1,8 @@
-console.dir(document)
-console.log(document.domain)
-console.log(document.title)
-document.title = "123"
-console.log(document.doctype)
+// console.dir(document)
+// console.log(document.domain)
+// console.log(document.title)
+// document.title = "123"
+// console.log(document.doctype)
 
 //getElementbyID//
 
@@ -16,10 +16,24 @@ addItem.style.fontWeight="Bold"
 addItem.style.color="green"
 */
 
-var items = document.getElementsByClassName('list-group-item');
+// var items = document.getElementsByClassName('list-group-item');
 
-items[2].style.backgroundColor = "green"
+// items[2].style.backgroundColor = "green"
 
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight="bold"
-}
+// for(let i=0;i<items.length;i++){
+//     items[i].style.fontWeight="bold"
+// }
+
+
+//tagName
+var li = document.getElementsByTagName('li');
+
+console.log(li)
+
+li[1].textContent = "Hello 2";
+li[1].style.fontWeight = "bold";
+li[1].style.backgroundColor = 'yellow'
+
+for(let i=0;i<li.length;i++){
+     li[i].style.backgroundColor="#f4f4f4";
+ }
